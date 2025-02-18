34b028254ba8489213fdabca4ac68e266222933f
Freadam Abebe <freadamabebe@gmail.com>
2025-02-18 12:17:23 +0300

    Add relationship app

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,26 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+  
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return f"No author found with name {author_name}"
+    
+def get_books_by_Library(libraryName):
+    try:
+        library = Library.objects.get(name=libraryName)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return f"No library found with name {libraryName}"
+
+def get_Librarian_by_Library(libraryName):
+    try:
+        library = Library.objects.get(name=libraryName)
+        librarian = library.librarian
+        return librarian
+    except Library.DoesNotExist:
+        return f"No library found with name {libraryName}"
