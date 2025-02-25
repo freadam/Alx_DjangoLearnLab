cfe8dc8ad3d06a40ceeb00ba4276d53c7ee63af5
Freadam Abebe <freadamabebe@gmail.com>
2025-02-25 16:02:12 +0300

    Add authentication views and urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import list_books, LibraryDetailView  # Import list_books and LibraryDetailView
+from .views import list_books, LibraryDetailView,user_login,user_logout,register  # Import list_books and LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # URL for function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for class-based view
-    
+    path('login/', user_login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', user_logout, name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,3 +20,35 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+# User Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form}) 
+
+# User Login View
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')  # Redirect to a home page or dashboard
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# User Logout View
+def user_logout(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
