f2ac230d8468da346c5aaaba6c9b817d106e91fc
Freadam Abebe <freadamabebe@gmail.com>
2025-02-25 12:34:54 +0300

    Add Django views

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book, Library,Author
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author')  # Display the id field
+
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Display the id field
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Library, LibraryAdmin)
+admin.site.register(Author)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library  # Adjust the model import as needed
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def library_details(request, id):
+    library = get_object_or_404(Library, id=id)  # Get the library with the given id
+    return render(request, 'relationship_app/library_details.html', {'library': library})
+
+
+# relationship_app/views.py
+from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from .models import Library  # Adjust the model import as needed
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
