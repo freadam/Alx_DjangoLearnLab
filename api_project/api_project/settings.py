8b85065289571e6098b01703c33b767885b80409
Freadam Abebe <freadamabebe@gmail.com>
2025-03-07 12:57:20 +0300

    added DRF authentication to settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Use Token Authentication
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Require authentication for all views by default
+    ],
+}
+
