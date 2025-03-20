856e160a58b8d614091492d3a99b4c42f78b718d
Freadam Abebe <freadamabebe@gmail.com>
2025-03-20 17:48:37 +0300

    update db values

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres', 
+        'PASSWORD': 'admin1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
