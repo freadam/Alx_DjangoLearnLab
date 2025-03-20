f40ae4d5a84868a5681bae7cf28138b9cb4e6ca4
Freadam Abebe <freadamabebe@gmail.com>
2025-03-20 17:42:11 +0300

    change database to sqlite

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'alx',
-        'USER': 'postgres', 
-        'PASSWORD': '1853',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
